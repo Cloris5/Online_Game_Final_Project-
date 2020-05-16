32a4e5ccb7207a61257583c895ee190e3bfd4f2e
tulipblack <44391779+tulipblack@users.noreply.github.com>
2020-05-16 15:50:12 +0800

    AddFan

---
+++ Online_Game_Final_Project/Assets/Scripts/ZMDFan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZMDFan : MonoBehaviour
+{
+    int speed = 40;
+    void Start()
+    {
+
+
+    }
+
+
+    public void Update()
+    {
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+
+    }
+}
