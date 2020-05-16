8126541b0c3743345abf04324c4d240ce4a3ddc5
MediocreMinute <56686117+MediocreMinute@users.noreply.github.com>
2020-05-16 18:52:21 +0800

    LFZ first update

---
+++ Online_Game_Final_Project/Assets/Scripts/LFZ_Cloud.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LFZ_Cloud : MonoBehaviour
+{
+    [SerializeField]//we gonna expose this variable in the editor 
+    private float speed;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = new Vector3(0, 0, speed);
+        Invoke("FallDestroy", 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FallDestroy()
+    {
+        rb.useGravity = true;
+        Destroy(transform.gameObject, 0.5f);
+    }
+}
---
+++ Online_Game_Final_Project/Assets/Scripts/LFZ_CloudGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LFZ_CloudGenerator : MonoBehaviour
+{
+    public GameObject platform;
+    Vector3 pos;
+    public static LFZ_CloudGenerator instance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = platform.transform.position;
+
+        //随机
+
+        InvokeRepeating("SpawnPlatform", 1f, 7f);
+
+    }
+
+
+    void SpawnPlatform()
+    {
+        Instantiate(platform, pos, Quaternion.identity);
+    }
+
+
+}
